d5767db805d9dfa002b5277ac1bdad44a67ba45a
arturoc <arturo@openframeworks.cc>
2012-07-16 19:33:03 +0200

    example videorate

---
+++ example-videorate/src/testApp.cpp
@@ -0,0 +1,87 @@
+#include "testApp.h"
+using namespace ofxPm;
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	gui.setup();
+	gui.add(fps.setup("fps",30,1,200));
+
+	grabber.initGrabber(640,480);
+	vRate.setup(grabber,fps);
+	vBuffer.setup(vRate,400);
+	vHeader.setup(vBuffer);
+	vHeader.setDelayMs(1000);
+
+	vRenderer1.setup(vRate);
+	vRenderer2.setup(vHeader);
+	ofEnableAlphaBlending();
+	ofSetVerticalSync(true);
+	ofBackground(0);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	grabber.update();
+	vRate.setFps(fps);
+	vHeader.setFps(fps);
+	vHeader.setDelayMs(1000);
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	vBuffer.draw();
+	vHeader.draw();
+	vRenderer1.draw();
+	ofPushMatrix();
+	ofTranslate(640,0);
+	vRenderer2.draw();
+	ofPopMatrix();
+	gui.draw();
+	ofDrawBitmapString("FPS: " + ofToString(int(ofGetFrameRate()))
+			+ " || cameraBuffer FPS " + ofToString(vBuffer.getRealFPS()),20,ofGetHeight()-80);
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ example-videorate/src/testApp.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "ofMain.h"
+#include "VideoBuffer.h"
+#include "VideoHeader.h"
+#include "VideoGrabber.h"
+#include "VideoRenderer.h"
+#include "VideoRate.h"
+#include "BasicVideoRenderer.h"
+#include "ofxGui.h"
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+		ofxPm::VideoGrabber grabber;
+		ofxPm::VideoBuffer vBuffer;
+		ofxPm::VideoHeader vHeader;
+		ofxPm::BasicVideoRenderer vRenderer1,vRenderer2;
+		ofxPm::VideoRate vRate;
+
+		ofxPanel gui;
+		ofxIntSlider fps;
+
+};
